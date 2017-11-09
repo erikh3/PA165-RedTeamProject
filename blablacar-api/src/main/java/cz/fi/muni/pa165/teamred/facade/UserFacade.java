fff74724cb9471e2688045a708e8f9de331941c9
Jozef Cibik <jozefcibik@gmail.com>
2017-11-09 21:12:52 +0100

    Started Facade, Service and DTO implementation, Modified User Entiti to not implement Serializable

--- blablacar-api/src/main/java/cz/fi/muni/pa165/teamred/facade/UserFacade.java
+++ blablacar-api/src/main/java/cz/fi/muni/pa165/teamred/facade/UserFacade.java
@@ -1,5 +1,28 @@
 package cz.fi.muni.pa165.teamred.facade;
 
-// todo
+import cz.fi.muni.pa165.teamred.dto.UserCreateDTO;
+import cz.fi.muni.pa165.teamred.dto.UserDTO;
+
+import java.util.Collection;
+
+/**
+ * @author Jozef Cibík
+ */
 public interface UserFacade {
+
+    public Long createUser(UserCreateDTO userDTO);
+    public void editUser(UserDTO userDTO);
+    public void removeUser(Long userId);
+
+    public void addComment(Long userId, Long commentId);
+    public void addRideAsDriver(Long userId, Long rideId);
+    public void addRideAsPassenger(Long userId, Long rideId);
+
+    public void removeComment(Long userId, Long commentId);
+    public void removeRideAsDriver(Long userId, Long rideId);
+    public void removeRideAsPassenger(Long userId, Long rideId);
+
+    Collection<UserDTO> getAllUsers();
+    UserDTO findUserById(Long userId);
+    UserDTO findUserByNickname(String name);
 }
--- blablacar-service/src/main/java/cz/fi/muni/pa165/teamred/service/UserService.java
+++ blablacar-service/src/main/java/cz/fi/muni/pa165/teamred/service/UserService.java
@@ -1,8 +1,30 @@
 package cz.fi.muni.pa165.teamred.service;
 
+import cz.fi.muni.pa165.teamred.entity.Comment;
+import cz.fi.muni.pa165.teamred.entity.Ride;
+import cz.fi.muni.pa165.teamred.entity.User;
 import org.springframework.stereotype.Service;
 
-// todo
+import java.util.Collection;
+
+/**
+ * @author Jozef Cibík
+ */
 @Service
 public interface UserService {
+    User createUser(User user) throws IllegalArgumentException;
+    void editUser(User user) throws IllegalArgumentException;
+    void removeUser(User user) throws IllegalArgumentException;
+
+    void addUserRideAsPassenger(User user, Ride ride) throws IllegalArgumentException;
+    void addUserRideAsDriver(User user, Ride ride) throws IllegalArgumentException;
+    void addUserComment(User user, Comment comment) throws IllegalArgumentException;
+
+    void removeUserRideAsPassenger(User user, Ride ride) throws IllegalArgumentException;
+    void removeUserRideAsDriver(User user, Ride ride) throws IllegalArgumentException;
+    void removeUserComment(User user, Comment comment) throws IllegalArgumentException;
+
+    User findUserById(Long id)  throws IllegalArgumentException;
+    Collection<User> findAllUsers();
+    User findUserByNickname(String name) throws IllegalArgumentException;
 }
