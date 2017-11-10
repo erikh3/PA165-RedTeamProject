add3dfd7a22de2470c55192b4a98462c440538a8
Šimon Mačejovský <xmacejo2@fi.muni.cz>
2017-11-10 15:18:46 +0100

    RideServiceImpl

--- blablacar-service/src/main/java/cz/fi/muni/pa165/teamred/service/RideServiceImpl.java
+++ blablacar-service/src/main/java/cz/fi/muni/pa165/teamred/service/RideServiceImpl.java
@@ -1,8 +1,47 @@
 package cz.fi.muni.pa165.teamred.service;
 
+import cz.fi.muni.pa165.teamred.dao.CommentDao;
+import cz.fi.muni.pa165.teamred.dao.RideDao;
+import cz.fi.muni.pa165.teamred.entity.Comment;
+import cz.fi.muni.pa165.teamred.entity.Ride;
 import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import java.util.List;
 
 // todo
 @Service
 public class RideServiceImpl implements RideService {
+
+    @Inject
+    private RideDao rideDao;
+
+
+    @Override
+    public Ride createRide(Ride ride) throws IllegalArgumentException {
+        rideDao.create(ride);
+        return ride;
+    }
+
+    @Override
+    public void updateRide(Ride ride) throws IllegalArgumentException {
+        rideDao.update(ride);
+
+    }
+
+    @Override
+    public void deleteRide(Ride ride) throws IllegalArgumentException {
+        rideDao.delete(ride);
+
+    }
+
+    @Override
+    public Ride findById(Long id) throws IllegalArgumentException {
+        return rideDao.findById(id);
+    }
+
+    @Override
+    public List<Ride> findAll() {
+        return rideDao.findAll();
+    }
 }
